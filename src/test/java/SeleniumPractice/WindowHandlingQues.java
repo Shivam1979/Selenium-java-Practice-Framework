aabb443c82b2d28f1427673f2a7bdbd2c6c5ddd5
Shivam Singh <shivamsingh999@Shivams-MacBook-Air.local>
2025-11-25 00:48:53 +0530

    adding window handling filr

---
+++ src/test/java/SeleniumPractice/WindowHandlingQues.java
@@ -0,0 +1,25 @@
+package SeleniumPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHandlingQues {
+    static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/browser-windows");
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+       WebElement NewTabBtn = driver.findElement(By.xpath("//button[@id='tabButton']"));
+       NewTabBtn.click();
+      WebElement NewTabText = driver.findElement(By.id("sampleHeading"));
+        System.out.println(NewTabText.getText());
+
+    }
+}
